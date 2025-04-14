c064511ed95e4adfd1ae81bd1373708cd0fa0194
CGTech242 <cg4engineering@gmail.com>
2025-04-14 13:14:38 +0800

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1974824598"
+    serviceWorkerVersion: "3546786328"
   }
 });
